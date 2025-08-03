988fd46b775782b1df392116f10d9b44123b54f6
Brandon Szlago <brandon.szlago@hotmail.com>
2025-08-03 17:38:50 -0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { Route, Routes } from 'react-router'
 import App from './App.jsx'
 import './index.css'
-import ContactContextProvider from './Context/ContactContext.jsx'
+import ContactContextProvider from '.ContactContext.jsx'
 import AppThemeContextProvider from './Context/AppThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
